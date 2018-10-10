32caf023e87c8dfc72ea50c95183eb72bc8d44c9
Erika Miguel <ms.erikamiguel@gmail.com>
2018-10-09 20:38:48 -0400

    simple sign in page

---
+++ src/components/SignIn.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class SignIn extends Component {
+	render() {
+		const { users } = this.props
+		return (
+			<div className="sign-in-page">
+				<h1>Welcome to the Would You Rather App!</h1>
+				<p>Please sign in to continue</p>
+				<select className="sign-in-select">
+					{
+						users.map(user => ((
+							<option key={user.id} value={user.id}>{user.name}</option>
+						)))
+					}
+				</select>
+			</div>
+		)
+	}
+}
+
+function mapStateToProps({users}) {
+	return {
+		users: Object.values(users)
+	}
+}
+
+export default connect(mapStateToProps)(SignIn)
