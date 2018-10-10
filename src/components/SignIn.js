211eee7fc280c782eb9795fb142c0082e646ac08
Erika Miguel <ms.erikamiguel@gmail.com>
2018-10-10 13:56:21 -0400

    minor: using react state to pass into redux state

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import './css/SignIn.css'
+import { setAuthedUser } from '../actions/authedUser'
 
 class SignIn extends Component {
+	state = {
+		authedUser: 'Select...'
+	}
+
+	changeUser = (event) => {
+		this.setState({authedUser: event.target.value})
+	}
+
+	authorizeUser = () => {
+		console.log(this.state.authedUser)
+	}
+
 	render() {
 		const { users } = this.props
 		return (
@@ -12,14 +25,14 @@ class SignIn extends Component {
 					<p className='sign-in-p'>Please sign in to continue</p>
 				</div>
 				<h1 className='sign-in-prompt-text'>Sign in</h1>
-				<select className='sign-in-select'>
+				<select className='sign-in-select' value={this.state.authedUser} onChange={this.changeUser}>
 					{
 						users.map(user => ((
 							<option key={user.id} value={user.id}>{user.name}</option>
 						)))
 					}
 				</select>
-				<button className='sign-in-btn'>Sign In</button>
+				<button className='sign-in-btn' onClick={this.authorizeUser}>Sign In</button>
 			</div>
 		)
 	}
