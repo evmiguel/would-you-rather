a998c846d3b03b79c63770639223ccab35d98cc6
Erika Miguel <ms.erikamiguel@gmail.com>
2018-10-11 12:54:17 -0400

    minor feat: preliminary functionality to get answered questions

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 class Home extends Component {
-	componentDidMount() {
-		console.log(this.props)
-	}
 	render() {
-		return(
-			<div>
-				HOME
-			</div>
-		)
+		const { questions, users, authedUser } = this.props
+		// Promp user to sign in on page
+        if(!authedUser) {
+        	return <Redirect to='/signin' />
+        }
+
+        let userAnswers = users[authedUser].answers
+        return (
+        	<div>
+        		<h1>Answered Questions</h1>
+        		<ul>
+        			{
+        				Object.keys(userAnswers).map(answer => (
+        					<li key={questions[answer].id}>{questions[answer].id}</li>
+        				))
+        			}
+        		</ul>
+        	</div>
+        )
 	}
 }
 
