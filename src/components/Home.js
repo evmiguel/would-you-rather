5d95d3bd761d310707bc337a64389da3b2b7c0b7
Erika Miguel <ms.erikamiguel@gmail.com>
2018-10-11 16:57:04 -0400

    minor feat: responsive home dashboard

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react'
 import { Redirect, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
 import WouldYouRatherCard from './WouldYouRatherCard'
+import './css/Home.css'
 
 class Home extends Component {
 	render() {
@@ -18,11 +19,11 @@ class Home extends Component {
 
         return (
         	<div className='home-dashboard'>
-	        	<div className='answered-questions'>
-	        		<h1>Answered Questions</h1>
+        		<div className='unanswered-questions'>
+	        		<h1>Unanswered Questions</h1>
 	        		<ul>
 	        			{
-	        				Object.keys(userAnswers).map(answer => (
+	        				Object.keys(unansweredQuestions).map(answer => (
 	        					<li key={questions[answer].id}>
 	        						<WouldYouRatherCard
 	        							author={users[questions[answer].author].name}
@@ -33,11 +34,11 @@ class Home extends Component {
 	        			}
 	        		</ul>
 	        	</div>
-	        	<div className='unanswered-questions'>
-	        		<h1>Unanswered Questions</h1>
+	        	<div className='answered-questions'>
+	        		<h1>Answered Questions</h1>
 	        		<ul>
 	        			{
-	        				Object.keys(unansweredQuestions).map(answer => (
+	        				Object.keys(userAnswers).map(answer => (
 	        					<li key={questions[answer].id}>
 	        						<WouldYouRatherCard
 	        							author={users[questions[answer].author].name}
--- src/components/WouldYouRatherCard.js
+++ src/components/WouldYouRatherCard.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import './css/WouldYouRather.css'
 
 const WouldYouRatherCard = (props) => {
+	// How much to subtract the first option to make a preview string
+	let toSubtract = (props.optionOne.length > 20) ? 12 : 5
 	return (
-		<div class="card">
+		<div className="card">
 			<h3>{props.author} asks:</h3>
 			<div className='question-box'>
 				<p>ICON</p>
 				<div className='question-text'>
 					<h4>Would you rather</h4>
-					<p>...{props.optionOne.substring(0, props.optionOne.length-5)}...</p>
+					<p>...{props.optionOne.substring(0, props.optionOne.length - toSubtract )}...</p>
 				</div>
 			</div>
 		</div>
