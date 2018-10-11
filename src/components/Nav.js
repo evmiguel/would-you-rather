29924e6e749e99cd6b404e09ce969abb641a6452
Erika Miguel <ms.erikamiguel@gmail.com>
2018-10-11 11:43:45 -0400

    minor feature: hammburger menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,37 @@
-import React, { Component } from 'react'
+import React, { Component, createRef } from 'react'
 import { NavLink } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 class Nav extends Component {
+  constructor(props) {
+    super(props)
+    this.outerDivNode = createRef()
+  }
+
+  state = {
+    menuNavOpen: false
+  }
+
+  hamburgerClick = () => {
+    this.setState({menuNavOpen : true})
+  }
+
+  componentDidMount() {
+    document.addEventListener('click', this.outerDivClick, false)
+  }
+
+  outerDivClick = (e) => {
+    if (!this.outerDivNode.current.contains(e.target)){
+      this.setState({menuNavOpen: false})
+    }
+  }
+
   render() {
     const { authedUser, users } = this.props
     return (
-      <div className='complete-nav'>
-        <nav className='nav'>
+      <div className='complete-nav' ref={this.outerDivNode}>
+        <a className='burger-nav' onClick={this.hamburgerClick}>&#9776;</a>
+        <nav className={`nav menu-nav ${this.state.menuNavOpen && 'open'}`}>
           <ul>
             <li>
               <NavLink to='/' exact >
@@ -28,7 +52,20 @@ class Nav extends Component {
         </nav>
         {
           !authedUser
-            ? null
+            ? <nav className='nav'>
+                <ul>
+                  <li className='placeholder-li'>
+                    Place
+                  </li>
+                  <li className='placeholder-li'>
+                    Holder
+                  </li>
+                  <li className='placeholder-li'>
+                    Placeholder
+                  </li>
+                </ul>
+              </nav>
+
             : <nav className='nav'>
                 <ul>
                   <li>
