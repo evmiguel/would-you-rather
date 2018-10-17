1f596860cc105584e63290bcd0d9f47a94cb1690
Erika Miguel <ms.erikamiguel@gmail.com>
2018-10-17 14:50:57 -0400

    minor feat: sorted leader board by score

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -11,7 +11,7 @@ const LeaderBoard = (props) => {
 						<h2>{user.name}</h2>
 						<h3>Answered Questions: {Object.keys(user.answers).length}</h3>
 						<h3>Questions: {user.questions.length}</h3>
-						<h3>Score: {Object.keys(user.answers).length + user.questions.length}</h3>
+						<h3>Score: {user.score}</h3>
 					</li>
 					)
 				}
@@ -21,8 +21,20 @@ const LeaderBoard = (props) => {
 }
 
 function mapStateToProps({users}){
+	// Make a new user object so that users doesn't get overwritten.
+	let userObj = Object.assign({}, users)
+	Object.values(users).map(user => userObj[user.id]['score'] = Object.keys(user.answers).length + user.questions.length)
 	return {
-		users: Object.values(users)
+		// Create an array of users, sorted by score
+		users: Object.values(userObj).sort((a,b) => {
+			if (a.score < b.score) {
+				return 1
+			} else if (a.score > b.score) {
+				return -1
+			} else {
+				return 0
+			}
+		})
 	}
 }
 
